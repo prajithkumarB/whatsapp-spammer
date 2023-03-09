f9ab1b7e5cb206522fc23dcd9a361b93dd50c16f
Prajith Kumar.B <codewithprajith@gmail.com>
2023-03-09 11:06:12 +0530

    Add files via upload

---
+++ Whatsapp Spammer.py
@@ -0,0 +1,22 @@
+#developed by @codewithprajith
+
+#Instructions 
+# 1. While entering the number of times you need to open the person's whatsapp chat and run the program
+
+# 2. Then just click on the text feild then the program will be automatically executed  
+
+import pyautogui as pt
+import time 
+
+limit =input("Enter the limit of message:")
+message = input("Enter the message to send:")
+i = 0
+
+time.sleep(3)
+
+while i<int(limit):
+    pt.typewrite(message)
+    pt.press("enter")
+    i+=1
+
+#developed by @codewithprajith
